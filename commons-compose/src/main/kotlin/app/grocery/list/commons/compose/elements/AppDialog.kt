d1d5570c497115d4e99fa8632655625e820946f3
denisarharov <dennis.arkharov@gmail.com>
2025-06-02 16:53:03 +0200

    refactoring: unified text button functionality

--- commons-compose/src/main/kotlin/app/grocery/list/commons/compose/elements/AppDialog.kt
+++ commons-compose/src/main/kotlin/app/grocery/list/commons/compose/elements/AppDialog.kt
@@ -17,13 +17,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -111,21 +109,15 @@ private fun Content(
                 .align(Alignment.End),
         ) {
             if (onCancel != null) {
-                TextButton(
+                AppTextButton(
+                    text = StringValue.ResId(android.R.string.cancel),
                     onClick = onCancel,
-                ) {
-                    Text(
-                        text = stringResource(android.R.string.cancel),
-                    )
-                }
-            }
-            TextButton(
-                onClick = onConfirm,
-            ) {
-                Text(
-                    text = confirmButtonText.value(),
                 )
             }
+            AppTextButton(
+                text = confirmButtonText,
+                onClick = onConfirm,
+            )
         }
     }
 }
---
+++ commons-compose/src/main/kotlin/app/grocery/list/commons/compose/elements/AppTextButton.kt
@@ -0,0 +1,38 @@
+package app.grocery.list.commons.compose.elements
+
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import app.grocery.list.commons.compose.theme.GroceryListTheme
+import app.grocery.list.commons.compose.theme.values.StringValue
+
+@Composable
+fun AppTextButton(
+    text: StringValue,
+    onClick: () -> Unit,
+) {
+    TextButton(
+        onClick = onClick,
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = MaterialTheme.colorScheme.secondary,
+        ),
+    ) {
+        Text(
+            text = text.value(),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AppTextButtonPreview() {
+    GroceryListTheme {
+        AppTextButton(
+            text = StringValue.StringWrapper("Text"),
+            onClick = {},
+        )
+    }
+}
