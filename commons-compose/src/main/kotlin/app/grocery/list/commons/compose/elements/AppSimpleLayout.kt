5285f083af75584a05709cf4ac717da84b8726ba
denisarharov <dennis.arkharov@gmail.com>
2025-07-04 15:02:39 +0200

    minor-issues: removed unnecessary code

--- commons-compose/src/main/kotlin/app/grocery/list/commons/compose/elements/AppSimpleLayout.kt
+++ commons-compose/src/main/kotlin/app/grocery/list/commons/compose/elements/AppSimpleLayout.kt
@@ -6,12 +6,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -83,15 +81,6 @@ private fun Content(
             }
         }
     }
-}
-
-@Composable
-private fun NavBarOffset() {
-    Spacer(
-        modifier = Modifier
-            .windowInsetsBottomHeight(WindowInsets.navigationBars)
-            .fillMaxWidth(),
-    )
 }
 
 @PreviewLightDark
