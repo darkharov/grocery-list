e37e68ba5a929b14ea9acd8dbb8e2ef90764d26e
denisarharov <dennis.arkharov@gmail.com>
2025-07-15 19:17:02 +0200

    version update 11 (1.5.2)

--- assembly/build.gradle.kts
+++ assembly/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = Configs.APPLICATION_ID
         minSdk = Configs.Sdk.MIN
         targetSdk = Configs.Sdk.TARGET
-        versionCode = 10
-        versionName = "1.5.1"
+        versionCode = 11
+        versionName = "1.5.2"
     }
 
     signingConfigs {
