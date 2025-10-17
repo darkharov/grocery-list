b44d47571188c4481d69c98a0d50fe0104f2d7d2
denisarharov <dennis.arkharov@gmail.com>
2025-10-17 18:51:53 +0200

    version update 29 (1.14.0)

--- assembly/build.gradle.kts
+++ assembly/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = Configs.APPLICATION_ID
         minSdk = Configs.Sdk.MIN
         targetSdk = Configs.Sdk.TARGET
-        versionCode = 28
-        versionName = "1.13.0"
+        versionCode = 29
+        versionName = "1.14.0"
     }
 
     signingConfigs {
