0492f8f14b1e77487326dcc77a6149a6e07eb2dd
denisarharov <dennis.arkharov@gmail.com>
2025-10-28 00:18:29 +0100

    lint: 'typealias MenuAnchorType = ExposedDropdownMenuAnchorType' is deprecated. Renamed to ExposedDropdownMenuAnchorType.

--- presentation/screens/product-input-form/src/main/kotlin/app/grocery/list/product/input/form/elements/category/picker/CategoryPicker.kt
+++ presentation/screens/product-input-form/src/main/kotlin/app/grocery/list/product/input/form/elements/category/picker/CategoryPicker.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -48,7 +48,7 @@ internal fun CategoryPicker(
             value = props.selectedCategory?.title.orEmpty(),
             onValueChange = {},
             modifier = Modifier
-                .menuAnchor(MenuAnchorType.PrimaryNotEditable)
+                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)
                 .fillMaxWidth()
                 .focusRequester(focusRequester)
                 .onFocusChanged {
