008dbbf99e8997bce678af3b0104157f2271151c
denisarharov <dennis.arkharov@gmail.com>
2025-11-19 22:03:23 +0100

    refactoring: simplified Holiday class

--- domain/src/main/kotlin/app/grocery/list/domain/Holiday.kt
+++ domain/src/main/kotlin/app/grocery/list/domain/Holiday.kt
@@ -2,12 +2,27 @@ package app.grocery.list.domain
 
 import org.joda.time.LocalDateTime
 
-sealed interface Holiday {
+sealed class Holiday(
+    private val emojis: List<String>,
+    private val month: Int,
+    private val days: IntRange,
+) {
+    fun emoji(): String? =
+        if (timeToChangeEmoji()) {
+            emojis.random()
+        } else {
+            null
+        }
 
-    fun timeToChangeEmoji(): Boolean
-    fun emoji(): String
+    private fun timeToChangeEmoji(): Boolean {
+        val now = LocalDateTime.now()
+        val currentMonth = now.monthOfYear
+        val currentDay = now.dayOfMonth
+        return currentMonth == month && days.contains(currentDay)
+    }
 
     companion object {
+
         private val entries = listOf(
             March8,
             AprilFoolsDay,
@@ -16,53 +31,30 @@ sealed interface Holiday {
         )
 
         fun alternativeEmoji(): String? =
-            entries
-                .find { it.timeToChangeEmoji() }
-                ?.emoji()
+            entries.firstNotNullOfOrNull { it.emoji() }
     }
 
-    private data object Halloween :
-        Holiday by SimpleHoliday(
-            emojis = listOf("🎃"),
-            month = 10,
-            days = (25..31),
-        )
+    private data object Halloween : Holiday(
+        emojis = listOf("🎃"),
+        month = 10,
+        days = (25..31),
+    )
 
-    private data object AprilFoolsDay :
-        Holiday by SimpleHoliday(
-            emojis = listOf("🤡"),
-            month = 4,
-            days = (1..1),
-        )
+    private data object AprilFoolsDay : Holiday(
+        emojis = listOf("🤡"),
+        month = 4,
+        days = (1..1),
+    )
 
-    private data object March8 :
-        Holiday by SimpleHoliday(
-            emojis = listOf("🌷", "💐"),
-            month = 3,
-            days = (7..8),
-        )
+    private data object March8 : Holiday(
+        emojis = listOf("🌷", "💐"),
+        month = 3,
+        days = (7..8),
+    )
 
-    private data object Christmas :
-        Holiday by SimpleHoliday(
-            emojis = listOf("🎄"),
-            month = 12,
-            days = (20..31),
-        )
-
-    private class SimpleHoliday(
-        private val emojis: List<String>,
-        private val month: Int,
-        private val days: IntRange? = null,
-    ) : Holiday {
-
-        override fun timeToChangeEmoji(): Boolean {
-            val now = LocalDateTime.now()
-            val currentMonth = now.monthOfYear
-            val currentDay = now.dayOfMonth
-            return currentMonth == month && (days?.contains(currentDay) ?: true)
-        }
-
-        override fun emoji(): String =
-            emojis.random()
-    }
+    private data object Christmas : Holiday(
+        emojis = listOf("🎄"),
+        month = 12,
+        days = (20..31),
+    )
 }
