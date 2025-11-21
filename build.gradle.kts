1caf6827935797353376a20292d8906126a94e8d
denisarharov <dennis.arkharov@gmail.com>
2025-11-21 16:24:16 +0100

    libs-version-update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-    id("com.google.dagger.hilt.android") version "2.56.1" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false
-    id("com.google.firebase.crashlytics") version "3.0.5" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.6" apply false
 }
 
 tasks.withType<KotlinJvmCompile>().configureEach {
