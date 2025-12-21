7c4c8fb050438248673b145a3034a23fe6ea6b31
denisarharov <dennis.arkharov@gmail.com>
2025-12-21 21:36:04 +0200

    minor-issues: removed unnecessary annotations

--- presentation/ui/main-activity/src/main/kotlin/app/grocery/list/main/activity/ui/content/AppEvent.kt
+++ presentation/ui/main-activity/src/main/kotlin/app/grocery/list/main/activity/ui/content/AppEvent.kt
@@ -1,11 +1,7 @@
 package app.grocery.list.main.activity.ui.content
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 sealed class AppEvent {
 
-    @Immutable
     data class PushNotificationsGranted(
         val clearNotificationsReminderEnabled: Boolean,
     ) : AppEvent()
