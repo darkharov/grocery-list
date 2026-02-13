128e6c14aceca1244ecdf412524982d40c87123a
denisarharov <dennis.arkharov@gmail.com>
2026-02-13 13:04:50 +0200

    version update 50 (1.19.2)

--- assembly/build.gradle.kts
+++ assembly/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = Configs.APPLICATION_ID
         minSdk = Configs.Sdk.MIN
         targetSdk = Configs.Sdk.TARGET
-        versionCode = 48
-        versionName = "1.19.0"
+        versionCode = 50
+        versionName = "1.19.2"
     }
 
     signingConfigs {
