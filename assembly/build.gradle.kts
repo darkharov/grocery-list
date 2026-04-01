232be738a2af8afe1acef5407647eb293564eacc
denisarharov <dennis.arkharov@gmail.com>
2026-04-01 21:57:01 +0300

    version update 60 (2.0.5)

--- assembly/build.gradle.kts
+++ assembly/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = Configs.APPLICATION_ID
         minSdk = Configs.Sdk.MIN
         targetSdk = Configs.Sdk.TARGET
-        versionCode = 59
-        versionName = "2.0.4"
+        versionCode = 60
+        versionName = "2.0.5"
     }
 
     signingConfigs {
