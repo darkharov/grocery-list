fbf12fb2c0c2ac63a522e75c99b5535b42ad2645
denisarharov <dennis.arkharov@gmail.com>
2026-04-02 17:57:35 +0300

    minor-issues: added alpha to AppUnderlinedTextButton

--- commons/compose/src/main/kotlin/app/grocery/list/commons/compose/elements/button/text/AppUnderlinedTextButton.kt
+++ commons/compose/src/main/kotlin/app/grocery/list/commons/compose/elements/button/text/AppUnderlinedTextButton.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
@@ -36,7 +37,8 @@ fun AppUnderlinedTextButton(
             .padding(
                 top = 10.dp,
                 bottom = 14.dp,
-            ),
+            )
+            .alpha(0.82f),
     )
 }
 
