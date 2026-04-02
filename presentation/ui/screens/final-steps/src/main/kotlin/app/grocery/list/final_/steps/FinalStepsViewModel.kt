3f20c6bab687a36ac5df3baa84ae92a426d06887
denisarharov <dennis.arkharov@gmail.com>
2026-04-02 19:11:44 +0300

    fix: final steps screen should be closed if there is no enabled products anymore

--- presentation/ui/screens/final-steps/src/main/kotlin/app/grocery/list/final_/steps/FinalStepsViewModel.kt
+++ presentation/ui/screens/final-steps/src/main/kotlin/app/grocery/list/final_/steps/FinalStepsViewModel.kt
@@ -2,18 +2,17 @@ package app.grocery.list.final_.steps
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import app.grocery.list.domain.product.ProductRepository
+import app.grocery.list.domain.product.AtLeastOneProductInCurrentListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class FinalStepsViewModel @Inject constructor(
-    private val repository: ProductRepository,
+    private val atLeastOneProductInCurrentList: AtLeastOneProductInCurrentListUseCase,
 ) : ViewModel() {
 
     private val events = Channel<Event>(
@@ -23,11 +22,13 @@ class FinalStepsViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            repository
-                .numberOfEnabled()
-                .filter { it == 0 }
-                .collect {
-                    events.trySend(Event.OnNoEnabledProductsAnymore)
+            atLeastOneProductInCurrentList
+                .execute(enabledOnly = true)
+                .collect { atLeastOne ->
+                    if (!(atLeastOne)) {
+                        events.trySend(Event.OnNoEnabledProductsAnymore)
+                        return@collect
+                    }
                 }
         }
     }
