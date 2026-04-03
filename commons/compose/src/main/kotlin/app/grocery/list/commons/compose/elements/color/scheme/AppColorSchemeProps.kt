007aeb5942fd02bd8544695ea78b9d065d6b2fcd
denisarharov <dennis.arkharov@gmail.com>
2026-04-04 01:06:30 +0300

    refactoring: simplified app colors functionality

--- commons/compose/src/main/kotlin/app/grocery/list/commons/compose/elements/color/scheme/AppColorSchemeProps.kt
+++ commons/compose/src/main/kotlin/app/grocery/list/commons/compose/elements/color/scheme/AppColorSchemeProps.kt
@@ -3,53 +3,71 @@ package app.grocery.list.commons.compose.elements.color.scheme
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import app.grocery.list.commons.compose.theme.AppColors
-import app.grocery.list.commons.compose.theme.BlueColor40
-import app.grocery.list.commons.compose.theme.BlueColor60
-import app.grocery.list.commons.compose.theme.BlueDarkColors
-import app.grocery.list.commons.compose.theme.BlueLightColors
-import app.grocery.list.commons.compose.theme.BrandColor40
-import app.grocery.list.commons.compose.theme.BrandColor60
-import app.grocery.list.commons.compose.theme.GreenColor40
-import app.grocery.list.commons.compose.theme.GreenColor60
-import app.grocery.list.commons.compose.theme.GreenDarkColors
-import app.grocery.list.commons.compose.theme.GreenLightColors
-import app.grocery.list.commons.compose.theme.MagentaColor40
-import app.grocery.list.commons.compose.theme.MagentaColor60
-import app.grocery.list.commons.compose.theme.MagentaDarkColors
-import app.grocery.list.commons.compose.theme.MagentaLightColors
-import app.grocery.list.commons.compose.theme.YellowDarkColors
-import app.grocery.list.commons.compose.theme.YellowLightColors
 
 @Immutable
 enum class AppColorSchemeProps(
-    val demoColor1: Color,
-    val demoColor2: Color,
-    val lightColors: AppColors,
-    val darkColors: AppColors,
+    val color00: Color,
+    val color10: Color,
+    val color20: Color,
+    val color30: Color,
+    val color40: Color,
+    val color50: Color,
+    val color60: Color,
+    val color70: Color,
+    val color80: Color,
+    val color90: Color,
 ) {
     Yellow(
-        demoColor1 = BrandColor40,
-        demoColor2 = BrandColor60,
-        lightColors = YellowLightColors,
-        darkColors = YellowDarkColors,
+        color00 = Color(0xFF_000000),
+        color10 = Color(0xFF_191300),
+        color20 = Color(0xFF_332600),
+        color30 = Color(0xFF_664c00),
+        color40 = Color(0xFF_b48700),
+        color50 = Color(0xFF_ffbf01),
+        color60 = Color(0xFF_ffcc34),
+        color70 = Color(0xFF_ffd65c),
+        color80 = Color(0xFF_fff4d6),
+        color90 = Color(0XFF_fffdfa),
     ),
     Blue(
-        demoColor1 = BlueColor40,
-        demoColor2 = BlueColor60,
-        lightColors = BlueLightColors,
-        darkColors = BlueDarkColors,
+        color00 = Color(0xFF_000000),
+        color10 = Color(0xFF_001319),
+        color20 = Color(0xFF_002633),
+        color30 = Color(0xFF_004c66),
+        color40 = Color(0xFF_0087b4),
+        color50 = Color(0xFF_01c0ff),
+        color60 = Color(0xFF_34cdff),
+        color70 = Color(0xFF_5cd7ff),
+        color80 = Color(0xFF_d6f5ff),
+        color90 = Color(0XFF_fafeff),
     ),
     Green(
-        demoColor1 = GreenColor40,
-        demoColor2 = GreenColor60,
-        lightColors = GreenLightColors,
-        darkColors = GreenDarkColors,
+        color00 = Color(0xFF_000000),
+        color10 = Color(0xFF_0f1302),
+        color20 = Color(0xFF_1f2705),
+        color30 = Color(0xFF_3e4f0a),
+        color40 = Color(0xFF_678310),
+        color50 = Color(0xFF_9cc719),
+        color60 = Color(0xFF_b8e52e),
+        color70 = Color(0xFF_c6ea57),
+        color80 = Color(0xFF_f0f9d5),
+        color90 = Color(0XFF_fdfefa),
     ),
     Magenta(
-        demoColor1 = MagentaColor40,
-        demoColor2 = MagentaColor60,
-        lightColors = MagentaLightColors,
-        darkColors = MagentaDarkColors,
+        color00 = Color(0xFF_000000),
+        color10 = Color(0xFF_331a2c),
+        color20 = Color(0xFF_4c2743),
+        color30 = Color(0xFF_7f4170),
+        color40 = Color(0xFF_ff69da),
+        color50 = Color(0xFF_ff82e0),
+        color60 = Color(0xFF_ff9be6),
+        color70 = Color(0xFF_ffafeb),
+        color80 = Color(0xFF_ffebfa),
+        color90 = Color(0XFF_fffdfe),
     ),
     ;
+    val demoColor1 = color40
+    val demoColor2 = color60
+    val lightColors = AppColors.light(this)
+    val darkColors = AppColors.dark(this)
 }
--- commons/compose/src/main/kotlin/app/grocery/list/commons/compose/theme/AppColors.kt
+++ commons/compose/src/main/kotlin/app/grocery/list/commons/compose/theme/AppColors.kt
@@ -3,8 +3,9 @@ package app.grocery.list.commons.compose.theme
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import app.grocery.list.commons.compose.elements.color.scheme.AppColorSchemeProps
 
-val LocalAppColors = staticCompositionLocalOf { YellowLightColors }
+val LocalAppColors = staticCompositionLocalOf { AppColorSchemeProps.Yellow.lightColors }
 
 @Suppress("PropertyName")
 @Immutable
@@ -29,164 +30,52 @@ data class AppColors(
 ) {
     val border = brand_50_50
     val background = brand_90_10
+
+    companion object {
+
+        fun light(colorScheme: AppColorSchemeProps) =
+            AppColors(
+                blackOrWhite = Color.Black,
+                whiteOrBlack = Color.White,
+                inactive1 = Inactive1Light,
+                inactive2 = Inactive2Light,
+                inactive3 = Inactive3Light,
+                brand_00_50 = colorScheme.color00,
+                brand_30_40 = colorScheme.color30,
+                brand_40_40 = colorScheme.color40,
+                brand_40_50 = colorScheme.color40,
+                brand_50_50 = colorScheme.color50,
+                brand_60_50 = colorScheme.color60,
+                brand_70_30 = colorScheme.color70,
+                brand_80_20 = colorScheme.color80,
+                brand_90_10 = colorScheme.color90,
+            )
+
+        fun dark(palette: AppColorSchemeProps) =
+            AppColors(
+                blackOrWhite = Color.White,
+                whiteOrBlack = Color.Black,
+
+                inactive1 = palette.color20,
+                inactive2 = palette.color30,
+                inactive3 = palette.color80,
+
+                brand_00_50 = palette.color50,
+                brand_30_40 = palette.color40,
+                brand_40_40 = palette.color40,
+                brand_40_50 = palette.color50,
+                brand_50_50 = palette.color50,
+                brand_60_50 = palette.color50,
+                brand_70_30 = palette.color30,
+                brand_80_20 = palette.color20,
+                brand_90_10 = palette.color10,
+            )
+    }
 }
 
-val YellowLightColors =
-    AppColors(
-        blackOrWhite = Color.Black,
-        whiteOrBlack = Color.White,
+val Inactive1Light = Color(0xFFF0F0F0)
+val Inactive2Light = Color(0xFFA0A0A0)
+val Inactive3Light = Color(0xFF424242)
 
-        inactive1 = Inactive1Light,
-        inactive2 = Inactive2Light,
-        inactive3 = Inactive3Light,
-
-        brand_00_50 = BrandColor00,
-        brand_30_40 = BrandColor30,
-        brand_40_40 = BrandColor40,
-        brand_40_50 = BrandColor40,
-        brand_50_50 = BrandColor50,
-        brand_60_50 = BrandColor60,
-        brand_70_30 = BrandColor70,
-        brand_80_20 = BrandColor80,
-        brand_90_10 = BrandColor90,
-    )
-
-val YellowDarkColors =
-    AppColors(
-        blackOrWhite = Color.White,
-        whiteOrBlack = Color.Black,
-
-        inactive1 = BrandColor20,
-        inactive2 = BrandColor30,
-        inactive3 = BrandColor80,
-
-        brand_00_50 = BrandColor50,
-        brand_30_40 = BrandColor40,
-        brand_40_40 = BrandColor40,
-        brand_40_50 = BrandColor50,
-        brand_50_50 = BrandColor50,
-        brand_60_50 = BrandColor50,
-        brand_70_30 = BrandColor30,
-        brand_80_20 = BrandColor20,
-        brand_90_10 = BrandColor10,
-    )
-
-val BlueLightColors =
-    AppColors(
-        blackOrWhite = Color.Black,
-        whiteOrBlack = Color.White,
-
-        inactive1 = Inactive1Light,
-        inactive2 = Inactive2Light,
-        inactive3 = Inactive3Light,
-
-        brand_00_50 = BlueColor00,
-        brand_30_40 = BlueColor30,
-        brand_40_40 = BlueColor40,
-        brand_40_50 = BlueColor40,
-        brand_50_50 = BlueColor50,
-        brand_60_50 = BlueColor60,
-        brand_70_30 = BlueColor70,
-        brand_80_20 = BlueColor80,
-        brand_90_10 = BlueColor90,
-    )
-
-val BlueDarkColors =
-    AppColors(
-        blackOrWhite = Color.White,
-        whiteOrBlack = Color.Black,
-
-        inactive1 = BlueColor20,
-        inactive2 = BlueColor30,
-        inactive3 = BlueColor80,
-
-        brand_00_50 = BlueColor50,
-        brand_30_40 = BlueColor40,
-        brand_40_40 = BlueColor40,
-        brand_40_50 = BlueColor50,
-        brand_50_50 = BlueColor50,
-        brand_60_50 = BlueColor50,
-        brand_70_30 = BlueColor30,
-        brand_80_20 = BlueColor20,
-        brand_90_10 = BlueColor10,
-    )
-
-val GreenLightColors =
-    AppColors(
-        blackOrWhite = Color.Black,
-        whiteOrBlack = Color.White,
-
-        inactive1 = Inactive1Light,
-        inactive2 = Inactive2Light,
-        inactive3 = Inactive3Light,
-
-        brand_00_50 = GreenColor00,
-        brand_30_40 = GreenColor30,
-        brand_40_40 = GreenColor40,
-        brand_40_50 = GreenColor40,
-        brand_50_50 = GreenColor50,
-        brand_60_50 = GreenColor60,
-        brand_70_30 = GreenColor70,
-        brand_80_20 = GreenColor80,
-        brand_90_10 = GreenColor90,
-    )
-
-val GreenDarkColors =
-    AppColors(
-        blackOrWhite = Color.White,
-        whiteOrBlack = Color.Black,
-
-        inactive1 = GreenColor20,
-        inactive2 = GreenColor30,
-        inactive3 = GreenColor80,
-
-        brand_00_50 = GreenColor50,
-        brand_30_40 = GreenColor40,
-        brand_40_40 = GreenColor40,
-        brand_40_50 = GreenColor50,
-        brand_50_50 = GreenColor50,
-        brand_60_50 = GreenColor50,
-        brand_70_30 = GreenColor30,
-        brand_80_20 = GreenColor20,
-        brand_90_10 = GreenColor10,
-    )
-
-val MagentaLightColors =
-    AppColors(
-        blackOrWhite = Color.Black,
-        whiteOrBlack = Color.White,
-
-        inactive1 = Inactive1Light,
-        inactive2 = Inactive2Light,
-        inactive3 = Inactive3Light,
-
-        brand_00_50 = MagentaColor00,
-        brand_30_40 = MagentaColor30,
-        brand_40_40 = MagentaColor40,
-        brand_40_50 = MagentaColor40,
-        brand_50_50 = MagentaColor50,
-        brand_60_50 = MagentaColor60,
-        brand_70_30 = MagentaColor70,
-        brand_80_20 = MagentaColor80,
-        brand_90_10 = MagentaColor90,
-    )
-
-val MagentaDarkColors =
-    AppColors(
-        blackOrWhite = Color.White,
-        whiteOrBlack = Color.Black,
-
-        inactive1 = MagentaColor20,
-        inactive2 = MagentaColor30,
-        inactive3 = MagentaColor80,
-
-        brand_00_50 = MagentaColor50,
-        brand_30_40 = MagentaColor40,
-        brand_40_40 = MagentaColor40,
-        brand_40_50 = MagentaColor50,
-        brand_50_50 = MagentaColor50,
-        brand_60_50 = MagentaColor50,
-        brand_70_30 = MagentaColor30,
-        brand_80_20 = MagentaColor20,
-        brand_90_10 = MagentaColor10,
-    )
+val PositiveActionColor = Color(0xFF_b8e52e)
+val NegativeActionColor = Color(0xFF_ff855c)
