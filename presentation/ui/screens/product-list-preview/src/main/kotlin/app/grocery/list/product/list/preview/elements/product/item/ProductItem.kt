e4f6ff4008c34c48770397ab2332fd8a9838cb0f
denisarharov <dennis.arkharov@gmail.com>
2026-04-29 10:55:01 +0300

    minor-issues: added alpha to product title

--- presentation/ui/screens/product-list-preview/src/main/kotlin/app/grocery/list/product/list/preview/elements/product/item/ProductItem.kt
+++ presentation/ui/screens/product-list-preview/src/main/kotlin/app/grocery/list/product/list/preview/elements/product/item/ProductItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -102,6 +103,7 @@ private fun Content(
         Text(
             text = props.title,
             modifier = Modifier
+                .alpha(0.81f)
                 .weight(1f),
             color = LocalAppColors.current.blackOrWhite,
         )
